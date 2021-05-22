1d87dc12954aec412fc97ff92917faa15947adbd
SanAlves <>
2021-05-22 14:07:59 +0000

    Formatei o Programa

--- main.cpp
+++ main.cpp
@@ -1,48 +1,53 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void troca (string *m, string ant, string nov, string *nm){
-  int contador = 0;
-  //cout <<"mensagem normal: " << m[1];
-  for(int i = 0; i < 100; i++){
-    if(m[i] == ant){
-      nm[i] = nov;
-      contador++;
+void Troca (string *m, int num, string ant, string nov, string *nm){
+
+  int contador = 0; //Variável para retornar números de trocas feitas
+
+    cout << "\n";
+
+    for(int i = 0; i < num; i++){
+      if(m[i] == ant){
+        nm[i] = nov;
+        contador++;
+      }
+        cout << nm[i]; //Apresenta a nova mensagem
     }
-    cout << m[i];
-  }
-  cout<< "contado: " << contador;
-  cout << "\n" << "mensagem normal nm:" << nm[0] << nm[1] << nm[2]<< nm[3]<< nm[4] << nm[5] << nm[6] << nm[7];
+  
+    cout << "\n";
+
+    for(int i = 0; i < num; i++){
+      cout << m[i]; //Apresenta a mensagem antiga
+    }
+    cout<< "\n" << contador; //Apresenta numero de trocas
 }
 
 
 int main() {
-  string texto;
-  string mensagem[100];
-  string Nmensagem[100];
+  string texto; 
+  string mensagem[500];
+  string Nmensagem[500];
   string ant, nov;
   
 
+  cout << "Digite uma mensagem de até 500 caracteres: ";
+    getline(cin,texto);
 
 
+  for(int i = 0; i < texto.length(); i++){
+    mensagem[i] = texto[i];
+    Nmensagem[i] = mensagem[i];  
+  }
 
-cout << "digite: ";
-cin >> texto;
+  cout << "Digite o caracter que deseja substituir: ";
+    cin >> ant;
+  cout << "Digite um novo caracter: ";
+    cin >> nov;
 
-for(int i = 0; i < texto.length(); i++){
-  mensagem[i] = texto[i];
-  Nmensagem[i] = texto[i];
-   
-}
-cout << "antigo: ";
-cin >> ant;
-cout << "novo: ";
-cin >> nov;
-
-
-  troca (mensagem,ant,nov,Nmensagem);
+  Troca (mensagem,texto.length(),ant,nov,Nmensagem);
   
-//cout << *mensagem << "\n" << mensagem;
- 
+
 }
